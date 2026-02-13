775459152affa519bb8223529c75d3254bce6c1c
Andrei Antonik <antonik.av@gmail.com>
2026-02-13 08:27:14 +0800

    add UnitTests for Domain

---
+++ tests/UnitTests/Domain/Entities/Project_CreateIssue.cs
@@ -0,0 +1,50 @@
+using AutoFixture.Xunit3;
+using ProjectTracker.Domain.Entities;
+using ProjectTracker.Domain.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace ProjectTracker.UnitTests.Domain.Entities;
+
+public class Project_CreateIssue
+{
+    [Theory]
+    [AutoData]
+    public void Create_issue_without_assignee(User owner)
+    {
+        var project = new Project("TP", "Test Project", owner);
+
+        var issue = project.CreateIssue(1, "test", owner);
+
+        issue.ShouldNotBeNull();
+        issue.Project.ShouldBe(project);
+        issue.Number.ShouldBe(1);
+        issue.Title.ShouldBe("test");
+        issue.Creator.ShouldBe(owner);
+        project.Issues.ShouldContain(issue);
+    }
+
+    [Theory]
+    [AutoData]
+    public void Create_issue_with_assignee(User owner, User memberAssignee)
+    {
+        var project = new Project("TP", "Test Project", owner);
+        project.Members.Add(memberAssignee);
+
+        var issue = project.CreateIssue(1, "test", owner, memberAssignee);
+
+        issue.Assignee.ShouldBe(memberAssignee);
+    }
+
+    [Theory]
+    [AutoData]
+    public void Throw_if_assignee_is_not_member(User owner, User nonMemberAssignee)
+    {
+        var project = new Project("TP", "Test Project", owner);
+
+        var exception = Should.Throw<AssigneeNotMemberException>(() =>
+            project.CreateIssue(1, "test", owner, nonMemberAssignee));
+
+        exception.AssigneeId.ShouldBe(nonMemberAssignee.Id);
+    }
+}
