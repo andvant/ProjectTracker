b98b5d14e88a1eb28f144da4458a2f4c1a80ceaf
Andrei Antonik <antonik.av@gmail.com>
2026-02-19 14:11:04 +0800

    add postgres to Aspire

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,6 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var database = builder.AddPostgres("postgres")
+    .WithImageTag("18")
+    .WithVolume("projecttracker_postgres_data", "/var/lib/postgresql")
+    .WithHostPort(5432)
+    .AddDatabase("ProjectTrackerDb");
+
 builder.AddProject<Projects.Api>("api")
-    .WithHttpEndpoint(5050);
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
