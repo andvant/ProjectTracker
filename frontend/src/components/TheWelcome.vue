f6d26b4d69e43afd6dc0003764365b8e214a80b8
Andrei Antonik <antonik.av@gmail.com>
2026-02-24 07:51:43 +0800

    add frontend Vite + Vue app

---
+++ frontend/src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Documentation</template>
+    Hello
+  </WelcomeItem>
+</template>
